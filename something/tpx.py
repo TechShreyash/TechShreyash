a62195e288fa581191af170b0f8dfea7030b2049
TechShreyash <82265247+TechShreyash@users.noreply.github.com>
2023-10-20 21:03:20 +0530

    Update and rename tpx.py to gogo_cookie_gen.py

--- something/tpx.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-if "TpXAnime" in requests.get("https://hindisub.com/").text:
-    with open("tpx.txt", "w") as f:
-        f.write("Cloudflare is down")
-else:
-    with open("tpx.txt", "w") as f:
-        f.write("Cloudflare is up")
